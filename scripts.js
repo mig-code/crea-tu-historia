43dfcfb832ed6426f30a49e77cbf8f54674927d6
Miguel <99726342+mig-code@users.noreply.github.com>
2022-06-06 20:17:45 +0200

    Mobile version
    Updating mobile UI

--- scripts.js
+++ scripts.js
@@ -1,61 +1,68 @@
 const boxContent =document.getElementById("main-content")
 const btnRender = document.getElementById("btn-render")
+let rand
 
-let rand =5
 
 function renderImages(){
-    boxContent.innerHTML=""
+    
+    renderLoading()
+
     fetch("https://api.arasaac.org/api/pictograms/all/es")
     .then(res=> res.json())
     .then(data=>{
+        boxContent.innerHTML= ""
         let maxRandom =data.length
-        let rand =Math.floor(Math.random()*maxRandom) 
+        
+        getRandomNumber(maxRandom)
+        AddBox(data[rand].keywords[0].keyword, data[rand]._id)
 
-        //THIS SHOULD BE IN A FUNCTION OUTSIDE
-        
-        boxContent.innerHTML +=`
-            <div id="box-content">
-                <div id="box-text">
-                    <p>${(data[rand].keywords[0].keyword).toUpperCase()}</p>
-                </div>
-            
-                <div id="box-image">
-                    <img src=https://static.arasaac.org/pictograms/${data[rand]._id}/${data[rand]._id}_500.png>
-                </div>
-            
-            </div>`
-        
-        rand =Math.floor(Math.random()*maxRandom) 
-        boxContent.innerHTML +=`
-            <div id="box-content">
-                <div id="box-text">
-                    <p>${(data[rand].keywords[0].keyword).toUpperCase()}</p>
-                </div>
-            
-                <div id="box-image">
-                    <img src=https://static.arasaac.org/pictograms/${data[rand]._id}/${data[rand]._id}_500.png>
-                </div>
-            
-            </div>`
-            
-        rand =Math.floor(Math.random()*maxRandom) 
-    boxContent.innerHTML +=`
-            <div id="box-content">
-                <div id="box-text">
-                    <p>${(data[rand].keywords[0].keyword).toUpperCase()}</p>
-                </div>
-            
-                <div id="box-image">
-                    <img src=https://static.arasaac.org/pictograms/${data[rand]._id}/${data[rand]._id}_500.png>
-                </div>
-            
-            </div>`
-        
+        getRandomNumber(maxRandom)
+        AddBox(data[rand].keywords[0].keyword, data[rand]._id)
 
+        getRandomNumber(maxRandom)
+        AddBox(data[rand].keywords[0].keyword, data[rand]._id)
+        
     })
     .catch(error => console.error('Error:', error))
 }
 
 btnRender.addEventListener("click",renderImages)
 
-renderImages()
+window.onload=renderImages()
+
+
+function renderLoading(){
+    return boxContent.innerHTML= `
+    <section id="main-content">
+    <div id="box-content">
+        <div id="box-text">
+            <p>CARGANDO</p>
+        </div>
+    
+        <div id="box-image">
+            <img id="load-animation" src=https://static.arasaac.org/pictograms/16091/16091_500.png>
+        </div>
+    
+    </div>
+    </section>`
+}
+
+function getRandomNumber(maxNum){
+    return rand = Math.floor(Math.random()*maxNum) 
+
+}
+
+function AddBox(title, imgId){
+
+    boxContent.innerHTML +=`
+        <div id="box-content">
+            <div id="box-text">
+                <p>${title.toUpperCase()}</p>
+            </div>
+        
+            <div id="box-image">
+                <img src=https://static.arasaac.org/pictograms/${imgId}/${imgId}_500.png>
+            </div>
+        
+        </div>`
+}
