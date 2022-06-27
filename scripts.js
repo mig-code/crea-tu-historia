645b3dc9e4bfe012ecc0819f5f2717dd6589cebf
Miguel <99726342+mig-code@users.noreply.github.com>
2022-06-27 18:41:11 +0200

    Solved
    Enabled and disabled the button when is loading.

--- scripts.js
+++ scripts.js
@@ -1,31 +1,32 @@
-const boxContent =document.getElementById("main-content")
-const btnRender = document.getElementById("btn-render")
-let rand
+const boxContent = document.getElementById("main-content");
+const btnRender = document.getElementById("btn-render");
+let rand;
 
+function renderImages() {
+  renderLoading();
+  btnRender.disabled = true;
 
-function renderImages(){
-    
-    renderLoading()
+  fetch("https://api.arasaac.org/api/pictograms/all/es")
+    .then((res) => res.json())
+    .then((data) => {
+      boxContent.innerHTML = "";
+      let maxRandom = data.length;
 
-    fetch("https://api.arasaac.org/api/pictograms/all/es")
-    .then(res=> res.json())
-    .then(data=>{
-        boxContent.innerHTML= ""
-        let maxRandom =data.length
-        
-        getRandomNumber(maxRandom)
-        AddBox(data[rand].keywords[0].keyword, data[rand]._id)
+      getRandomNumber(maxRandom);
+      AddBox(data[rand].keywords[0].keyword, data[rand]._id);
 
-        getRandomNumber(maxRandom)
-        AddBox(data[rand].keywords[0].keyword, data[rand]._id)
+      getRandomNumber(maxRandom);
+      AddBox(data[rand].keywords[0].keyword, data[rand]._id);
 
-        getRandomNumber(maxRandom)
-        AddBox(data[rand].keywords[0].keyword, data[rand]._id)
-        
+      getRandomNumber(maxRandom);
+      AddBox(data[rand].keywords[0].keyword, data[rand]._id);
+
+      btnRender.disabled = false;
     })
-    .catch(error => console.error('Error:', error))
+    .catch((error) => console.error("Error:", error));
 }
 
+<<<<<<< Updated upstream
 btnRender.addEventListener("click",renderImages)
 
 window.onload=renderImages()
@@ -33,11 +34,16 @@ window.onload=renderImages()
 
 function renderLoading(){
     return boxContent.innerHTML= `
+=======
+function renderLoading() {
+  return (boxContent.innerHTML = `
+>>>>>>> Stashed changes
     <section id="main-content">
     <div id="box-content">
         <div id="box-text">
             <p>CARGANDO</p>
         </div>
+<<<<<<< Updated upstream
     
         <div id="box-image">
             <img id="load-animation" src=https://static.arasaac.org/pictograms/16091/16091_500.png>
@@ -45,16 +51,17 @@ function renderLoading(){
     
     </div>
     </section>`
+=======
+    </section>`);
+>>>>>>> Stashed changes
 }
 
-function getRandomNumber(maxNum){
-    return rand = Math.floor(Math.random()*maxNum) 
-
+function getRandomNumber(maxNum) {
+  return (rand = Math.floor(Math.random() * maxNum));
 }
 
-function AddBox(title, imgId){
-
-    boxContent.innerHTML +=`
+function AddBox(title, imgId) {
+  boxContent.innerHTML += `
         <div id="box-content">
             <div id="box-text">
                 <p>${title.toUpperCase()}</p>
@@ -64,5 +71,9 @@ function AddBox(title, imgId){
                 <img src=https://static.arasaac.org/pictograms/${imgId}/${imgId}_500.png>
             </div>
         
-        </div>`
+        </div>`;
 }
+
+btnRender.addEventListener("click", renderImages);
+
+window.onload = renderImages();
