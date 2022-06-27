e1f7e0e40499014f6df114f86f4c9ec247066d8e
Miguel <99726342+mig-code@users.noreply.github.com>
2022-06-27 19:27:28 +0200

    Boton disable solved
    Disable botton when loading

--- scripts.js
+++ scripts.js
@@ -1,29 +1,29 @@
-const boxContent =document.getElementById("main-content")
-const btnRender = document.getElementById("btn-render")
-let rand
+const boxContent = document.getElementById("main-content");
+const btnRender = document.getElementById("btn-render");
+let rand;
 
+function renderImages() {
+  renderLoading();
+  btnRender.disabled = true;
 
-function renderImages(){
-    
-    renderLoading()
+  fetch("https://api.arasaac.org/api/pictograms/all/es")
+    .then((res) => res.json())
+    .then((data) => {
+      boxContent.innerHTML = "";
+      let maxRandom = data.length;
 
-    fetch("https://api.arasaac.org/api/pictograms/all/es")
-    .then(res=> res.json())
-    .then(data=>{
-        boxContent.innerHTML= ""
-        let maxRandom =data.length
-        
-        getRandomNumber(maxRandom)
-        AddBox(data[rand].keywords[0].keyword, data[rand]._id)
+      getRandomNumber(maxRandom);
+      AddBox(data[rand].keywords[0].keyword, data[rand]._id);
 
-        getRandomNumber(maxRandom)
-        AddBox(data[rand].keywords[0].keyword, data[rand]._id)
+      getRandomNumber(maxRandom);
+      AddBox(data[rand].keywords[0].keyword, data[rand]._id);
 
-        getRandomNumber(maxRandom)
-        AddBox(data[rand].keywords[0].keyword, data[rand]._id)
-        
+      getRandomNumber(maxRandom);
+      AddBox(data[rand].keywords[0].keyword, data[rand]._id);
+
+      btnRender.disabled = false;
     })
-    .catch(error => console.error('Error:', error))
+    .catch((error) => console.error("Error:", error));
 }
 
 btnRender.addEventListener("click",renderImages)
@@ -31,30 +31,28 @@ btnRender.addEventListener("click",renderImages)
 window.onload=renderImages()
 
 
-
 function renderLoading(){
     return boxContent.innerHTML= `
     <section id="main-content">
-        <div id="box-content">
-            <div id="box-text">
-                <p>CARGANDO</p>
-            </div>
-        
-            <div id="box-image">
-                <img id="load-animation" src=https://static.arasaac.org/pictograms/16091/16091_500.png>
-            </div>
+    <div id="box-content">
+        <div id="box-text">
+            <p>CARGANDO</p>
         </div>
+    
+        <div id="box-image">
+            <img id="load-animation" src=https://static.arasaac.org/pictograms/16091/16091_500.png>
+        </div>
+    
+    </div>
     </section>`
 }
 
-function getRandomNumber(maxNum){
-    return rand = Math.floor(Math.random()*maxNum) 
-
+function getRandomNumber(maxNum) {
+  return (rand = Math.floor(Math.random() * maxNum));
 }
 
-function AddBox(title, imgId){
-
-    boxContent.innerHTML +=`
+function AddBox(title, imgId) {
+  boxContent.innerHTML += `
         <div id="box-content">
             <div id="box-text">
                 <p>${title.toUpperCase()}</p>
@@ -64,5 +62,9 @@ function AddBox(title, imgId){
                 <img src=https://static.arasaac.org/pictograms/${imgId}/${imgId}_500.png>
             </div>
         
-        </div>`
+        </div>`;
 }
+
+btnRender.addEventListener("click", renderImages);
+
+window.onload = renderImages();
